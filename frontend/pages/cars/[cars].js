ee02d3d9a4301d12e0d8c119a408c212a9eb6d9b
vaibhav720 <parikhvaibhav19@gmail.com>
2022-11-15 23:31:35 +0530

    Others changes in landing page

--- frontend/pages/cars/[cars].js
+++ frontend/pages/cars/[cars].js
@@ -185,12 +185,10 @@ export default function Landing({ categories, articles, subCategories }) {
                     {articles.items[0].attributes.Title}
                   </h3>
                   <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                    This extension comes a lot of fully coded examples that help
-                    you get started faster. You can adjust the colors and also
-                    the programming language. You can change the text and images
-                    and you're good to go.
+                   All the Subcategories from which the data is divided is:
                   </p>
                   <ul className="list-none mt-6">
+                    {subCategories.items.map((item) => {
                     <li className="py-2">
                       <div className="flex items-center">
                         <div>
@@ -200,11 +198,12 @@ export default function Landing({ categories, articles, subCategories }) {
                         </div>
                         <div>
                           <h4 className="text-blueGray-500">
-                            Built by Developers for Developers
+                            {item.attributes.SubTitle}
                           </h4>
                         </div>
                       </div>
                     </li>
+                    })}
                     <li className="py-2">
                       <div className="flex items-center">
                         <div>
@@ -274,157 +273,8 @@ export default function Landing({ categories, articles, subCategories }) {
           </div>
 
           <Cards subCategories={subCategories} />
-          <div className="container mx-auto px-4">
-            <div className="items-center flex flex-wrap">
-              <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                <img
-                  alt="..."
-                  className="max-w-full rounded-lg shadow-lg"
-                  src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                />
-              </div>
-              <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-                <div className="md:pr-12">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blueGray-200">
-                    <i className="fas fa-rocket text-xl"></i>
-                  </div>
-                  <h3 className="text-3xl font-semibold">A growing company</h3>
-                  <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                    The extension comes with three pre-built pages to help you
-                    get started faster. You can change the text and images and
-                    you're good to go.
-                  </p>
-                  <ul className="list-none mt-6">
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fas fa-fingerprint"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Carefully crafted components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="fab fa-html5"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Amazing page examples
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-100 mr-3">
-                            <i className="far fa-paper-plane"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Dynamic components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+          
 
-          <div className="container mx-auto px-4 pb-32 pt-48">
-            <div className="items-center flex flex-wrap">
-              <div className="w-full md:w-5/12 ml-auto px-12 md:px-4">
-                <div className="md:pr-12">
-                  <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                    <i class="fas fa-solid fa-gauge-high"></i>
-                  </div>
-                  <h3 className="text-3xl font-semibold">Bugatti Bolide</h3>
-                  <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                    This extension comes a lot of fully coded examples that help
-                    you get started faster. You can adjust the colors and also
-                    the programming language. You can change the text and images
-                    and you're good to go.
-                  </p>
-                  <ul className="list-none mt-6">
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                            <i className="fas fa-fingerprint"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Built by Developers for Developers
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                            <i className="fab fa-html5"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Carefully crafted code for Components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                    <li className="py-2">
-                      <div className="flex items-center">
-                        <div>
-                          <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blueGray-500 bg-blueGray-50 mr-3">
-                            <i className="far fa-paper-plane"></i>
-                          </span>
-                        </div>
-                        <div>
-                          <h4 className="text-blueGray-500">
-                            Dynamic Javascript Components
-                          </h4>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                  <a
-                    href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/alerts/notus?ref=nnjs-index"
-                    target="_blank"
-                    className="font-bold text-blueGray-700 hover:text-blueGray-500 ease-linear transition-all duration-150"
-                  >
-                    View More{" "}
-                    <i className="fa fa-angle-double-right ml-1 leading-relaxed"></i>
-                  </a>
-                </div>
-              </div>
-
-              <div className="w-full md:w-6/12 mr-auto px-4 pt-24 md:pt-0">
-                <img
-                  alt="..."
-                  className="max-w-full rounded-lg shadow-xl"
-                  style={{
-                    transform:
-                      "scale(1) perspective(1040px) rotateY(-11deg) rotateX(2deg) rotate(2deg)",
-                  }}
-                  src="/img/bugatti-boldie.jpg"
-                />
-              </div>
-            </div>
-          </div>
         </section>
 
         <section className="pb-20 relative block bg-blueGray-800">
@@ -455,7 +305,7 @@ export default function Landing({ categories, articles, subCategories }) {
                   Build something
                 </h2>
                 <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">
-                  Put the potentially record low maximum sea ice extent tihs
+                  Put the potentially record low maximum sea ice extent this
                   year down to low ice. According to the National Oceanic and
                   Atmospheric Administration, Ted, Scambos.
                 </p>
